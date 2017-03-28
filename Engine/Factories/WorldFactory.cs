27d7d5d320ac2dd0c50d05b6ef3643fb4c6d0436
William Harrington <kb0iic@gmail.com>
2017-03-28 18:39:22 -0500

    Add remining location info for Trading Shop, Town Square, Town Gate, Spider Forest, Herbalist's Hut, and Herbalist's garden.

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -23,7 +23,31 @@ namespace Engine.Factories
 
             newWorld.AddLocation(0, -1, "Home",
                 "This is your home. It is full of spider webs.",
-                "/Engine;component/Images/Locations/Home.png"); 
+                "/Engine;component/Images/Locations/Home.png");
+
+            newWorld.AddLocation(-1, 0, "Trading Shop",
+                "The shop of Ole Ye Beggar.",
+                "/Engine;component/Images/Locations/Trader.png");
+
+            newWorld.AddLocation(0, 0, "Town square",
+                "You see a fountain and some shrubbery.",
+                "/Engine;component/Images/Locations/TownSquare.png");
+
+            newWorld.AddLocation(1, 0, "Town Gate",
+                "There is a gate protecting the town from giant arachnids.",
+                "Engine;component/Images/Locations/TownGate.png");
+
+            newWorld.AddLocation(2, 0, "Spider Forest",
+                "The trees in this forest are covered with spider webs.",
+                "/Engine;component/Images/Locations/SpiderForest.png");
+
+            newWorld.AddLocation(0, 1, "Herbalist's hut",
+                "You see a quaint hut, with vines covering it.",
+                "Engine;component/Images/Locations/HerbalistsHut.png");
+
+            newWorld.AddLocation(0, 2, "Herbalist's Garden",
+                "Plants provide refuge for the snakes inhabiting the garden.",
+                "Engine;component/Images/Locations/HerbalistsGarden.png");
 
             return newWorld;
         }
