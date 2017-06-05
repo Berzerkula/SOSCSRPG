45219a6f489191caa7ccab7a636ed5bbfeb419b4
William Harrington <kb0iic@gmail.com>
2017-06-05 11:44:31 -0500

    Add ItemFactory class to instantiate a game item object with CreateGameItem with itemTypeID as parameter. Create a list and use LINQ to query the list variable.

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>();
+
+            _standardGameItems.Add(new Weapon(1000, "Jagged Rock", 1, 1, 3));
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 2, 1, 5));
+            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 10));
+        }
+
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
+            if (_standardGameItems != null)
+            {
+            }
+        }
+    }
+}
