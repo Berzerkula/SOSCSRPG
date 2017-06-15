bb3f41ea8c2b2a43f73a7d160f405f599beaf77e
William Harrington <kb0iic@gmail.com>
2017-06-15 08:20:57 -0500

    Use Engine.Models and set class QuestFactory as internal and static.

--- Engine/Factories/QuestFactory.cs
+++ Engine/Factories/QuestFactory.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Engine.Models;
 
 namespace Engine.Factories
 {
-    class QuestFactory
+    internal static class QuestFactory
     {
     }
 }
