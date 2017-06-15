3fb10739f4e45e4e7277ac011c0768cde1ed9666
William Harrington <kb0iic@gmail.com>
2017-06-15 08:13:27 -0500

    Make Quest class public, add properties, and quest method.

--- Engine/Models/Quest.cs
+++ Engine/Models/Quest.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    class Quest
+    public class Quest
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<ItemQuantity> ItemsToComplete { get; set; }
+
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public List<ItemQuantity> RewardItems { get; set; }
+
+        public Quest(int id, string gname, string description, List<ItemQuantity> itemsToComplete,
+            int rewardExperiencePoints, int rewardGold, List<ItemQuantity> rewardItems)
+        {
+            ID = id;
+            Name = Name;
+            Description = description;
+            ItemsToComplete = itemsToComplete;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+            RewardItems = RewardItems;
+        }
     }
 }
