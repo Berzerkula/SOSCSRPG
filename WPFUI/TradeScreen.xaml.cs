2a5504d0d07596223181b2e653add6d527513853
William Harrington <kb0iic@gmail.com>
2018-03-07 16:35:32 -0600

    Update code for TradeScreen for OnClick Buy or Sell and Close actions.

--- WPFUI/TradeScreen.xaml.cs
+++ WPFUI/TradeScreen.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Engine.Models;
+using Engine.ViewModels;
 
 namespace WPFUI
 {
@@ -19,14 +9,47 @@ namespace WPFUI
     /// </summary>
     public partial class TradeScreen : Window
     {
+        public GameSession Session => DataContext as GameSession;
+
         public TradeScreen()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void OnClick_Sell(object sender, RoutedEventArgs e)
         {
+            GameItem item = ((FrameworkElement)sender).DataContext as GameItem;
 
+            if(item != null)
+            {
+                Session.CurrentPlayer.Gold += item.Price;
+                Session.CurrentTrader.AddItemToInventory(item);
+                Session.CurrentPlayer.RemoveItemFromInventory(item);
+            }
+        }
+
+        private void OnClick_Buy(object sender, RoutedEventArgs e)
+        {
+            GameItem item = ((FrameworkElement)sender).DataContext as GameItem;
+
+            if(item != null)
+            {
+                if(Session.CurrentPlayer.Gold >= item.Price)
+                {
+                    Session.CurrentPlayer.Gold -= item.Price;
+                    Session.CurrentTrader.RemoveItemFromInventory(item);
+                    Session.CurrentPlayer.AddItemToInventory(item);
+                }
+                else
+                {
+                    MessageBox.Show("You do not have enough gold");
+                }
+            }
+        }
+
+        private void OnClick_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
